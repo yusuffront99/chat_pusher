f78af30366e668d31c8e27dde3777404ce573880
yusuf <yusuffront@gmail.com>
2022-05-15 16:39:32 +0800

    updated several codes --ajax crud

---
+++ notify-pusher/app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Pusher\Pusher;
+use App\Models\Task;
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class TaskController extends Controller
+{
+    public function save_task(Request $request) {
+ 
+        $task = new Task;
+        $task->title = $request['title'];
+        $task->description = $request['description'];
+        $title = $request->title;
+
+
+        $message = new Message;
+        $message->from = Auth::user()->id;
+        $id = $message->from;
+        $message->message = $title;
+        $message->save();
+
+        $options = array(
+            'cluster' => 'ap1',
+            'useTLS' => true
+        );
+
+
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+
+
+        $data = ['from' => $id];
+        $pusher->trigger('my-channel', 'my-event', $data);
+
+
+        if($task->save()) {
+            return response()->json(['status' => true, 'message' => 'Task Added Successfully']);
+        }
+    }
+}
